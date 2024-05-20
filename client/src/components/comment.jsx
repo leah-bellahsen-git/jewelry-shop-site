1295ebd7cc6f5a5f56cee0e0c965980b49a4c990
leah-bellahsen-git <165821277+leah-bellahsen-git@users.noreply.github.com>
2024-05-20 15:25:02 +0300

    The first commit in commit edit

--- client/src/components/comment.jsx
+++ client/src/components/comment.jsx
@@ -1,20 +1,48 @@
 import { Editor } from "primereact/editor"
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { Tag } from "antd"
+import { useNavigate } from "react-router-dom";
 
 
 const Comment = ()=>{
 
-    const [text, setText] = useState('');
-    return(
+    const [close, setClose] = useState(false);
+    const [text, setText] = useState('We will always be happy to hear your comments and clarifications');
+    const navigate = useNavigate();
 
+    return(
+        
         <>
+        
+        
+         <br />
+         <br />
+         <br/>
+         {
+         !close &&
+        <h1 className="msg">We will always be happy to hear your comments and clarifications</h1>
+         }
         <br/>
         <br/>
-        <br/>
-       
-        <div className="card">
-            <Editor value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
+       {
+        !close && <div className="commit">
+            <Editor className="edit" style={{ fontSize: '16px', color: 'black', backgroundColor: 'lightgray', textAlign: 'center' }} value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
         </div>
+       }
+        
+           
+
+    <br/>
+
+
+    {close  && 
+        <h1 className="msg">Your commit send successfully! 😊</h1>}
+
+        {/* navigate('/homePage') */}
+        {
+            close && <button onClick={()=>{setClose(true);}}>Send</button>
+
+        }
         </>
     )
 }
