439634a0f14c0cc2b0c8edf2413f78a087bb3542
leah-bellahsen-git <165821277+leah-bellahsen-git@users.noreply.github.com>
2024-05-20 15:37:51 +0300

    Add some changes in commit.jsx file

--- client/src/components/comment.jsx
+++ client/src/components/comment.jsx
@@ -4,45 +4,50 @@ import { Tag } from "antd"
 import { useNavigate } from "react-router-dom";
 
 
-const Comment = ()=>{
+const Comment = () => {
 
     const [close, setClose] = useState(false);
     const [text, setText] = useState('We will always be happy to hear your comments and clarifications');
     const navigate = useNavigate();
 
-    return(
-        
+    return (
+
         <>
-        
-        
-         <br />
-         <br />
-         <br/>
-         {
-         !close &&
-        <h1 className="msg">We will always be happy to hear your comments and clarifications</h1>
-         }
-        <br/>
-        <br/>
-       {
-        !close && <div className="commit">
-            <Editor className="edit" style={{ fontSize: '16px', color: 'black', backgroundColor: 'lightgray', textAlign: 'center' }} value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
-        </div>
-       }
-        
-           
-
-    <br/>
 
 
-    {close  && 
-        <h1 className="msg">Your commit send successfully! 😊</h1>}
+            <br />
+            <br />
+            <br />
+            {
+                !close &&
+                <h1 className="msg">We will always be happy to hear your comments and clarifications</h1>
+            }
+            <br />
+            <br />
+            {
+                !close && <div className="commit">
+                    <Editor className="edit" style={{ fontSize: '16px', color: 'black', backgroundColor: 'lightgray', textAlign: 'center' }} value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
+                </div>
+            }
 
+
+
+            <br />
+
+
+            {close &&
+                <div className="fillDiv">
+                    <h1 className="msg">Your commit send successfully! 😊</h1>
+                </div>}
         {/* navigate('/homePage') */}
-        {
-            close && <button onClick={()=>{setClose(true);}}>Send</button>
+            {
+                !close && <button onClick={() => { setClose(true); }}>Send</button>
 
-        }
+            }
+            {
+                close &&
+                <button onClick={() => { navigate('/homePage') }}>Return to the home page</button>
+            }
         </>
     )
 }
